7efabdc12bebb16a56a6b2447cb5229c788d0bb4
Tao <tao@thebitstudio.com>
2017-03-21 16:28:11 +0700

    added experiment file structure

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
-import thalamus
+import src.thalamus as core
 
 inputs = np.where(np.random.rand(20, 100) > 0.5, np.ones((20, 100), dtype=np.float32), 0)
-machine = thalamus.Machine(100, 4, 3)
+machine = core.Machine(100, 4, 3)
 
 for i in xrange(4, inputs.shape[0]):
     pasts = inputs[i - 4:i, :]
@@ -10,6 +10,6 @@ for i in xrange(4, inputs.shape[0]):
     print machine.generate_thought(pasts)
     print "-----------"
     # when the generated thought is far from the example
-    machine.learn(input_data, pasts, "../artifacts/demo", 5)
+    machine.learn(input_data, pasts, "./artifacts/demo", 5)
     # otherwise memorize own thought for later use
-    machine.improve_thinking(pasts, "../artifacts/demo", 5)
+    machine.improve_thinking(pasts, "./artifacts/demo", 5)

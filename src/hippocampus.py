39bde4b089cb8b3ade18e27afc3d4c6579afc95c
Patrick Virie <p.virie@gmail.com>
2017-09-24 13:03:13 +0700

    handle component selection via inner response strength.

--- src/hippocampus.py
+++ src/hippocampus.py
@@ -5,7 +5,8 @@ import util
 
 class Transceducer:
 
-    def __init__(self, memory_size, context_size, recall_size):
+    def __init__(self, memory_size, context_size, recall_size, threshold):
+        self.threshold = threshold
         self.unit_sizes = (memory_size, context_size, recall_size)
         self.C = tf.Variable(util.random_uniform(context_size, memory_size), dtype=tf.float32)
         self.R = tf.Variable(util.random_uniform(recall_size, memory_size), dtype=tf.float32)
@@ -20,24 +21,23 @@ class Transceducer:
         self.reset_ops.append(tf.assign(self.C, util.random_uniform(context_size, memory_size)))
         self.reset_ops.append(tf.assign(self.R, util.random_uniform(recall_size, memory_size)))
 
-    def _filter(self, h):
-        h = tf.one_hot(tf.argmax(h, axis=1), self.unit_sizes[0], 1.0, 0.0)
-        return h
-
-    def _ceil(self, h):
-        return tf.tile(tf.reduce_max(h, axis=1, keep_dims=True), [1, self.unit_sizes[0]])
+    def _extract_winner(self, h):
+        val = tf.reduce_max(h, axis=1, keep_dims=True)
+        loc = tf.one_hot(tf.argmax(h, axis=1), self.unit_sizes[0], 1.0, 0.0)
+        return loc, val
 
     def infer(self, context):
-        h_ = self._filter(tf.matmul(context, self.C))
-        v_ = tf.matmul(h_, self.R, transpose_b=True)
+        loc, val = self._extract_winner(tf.matmul(context, self.C))
+        h_ = tf.multiply(loc, tf.where(val > self.threshold, val, tf.zeros(tf.shape(val))))
+        v_ = tf.matmul(tf.clip_by_value(h_, 0.0, 1.0), self.R, transpose_b=True)
         return v_
 
     def gradients(self, context, recall):
 
-        h_ = tf.nn.softmax(tf.matmul(context, self.C), dim=-1)
+        loc, val = self._extract_winner(tf.nn.softmax(tf.matmul(context, self.C), dim=-1))
         h = tf.where(
-            self._ceil(h_) > 0.8,
-            self._filter(h_),
+            tf.tile(val, [1, self.unit_sizes[0]]) > self.threshold,
+            loc,
             tf.tile(self.seed, [tf.shape(context)[0], 1]))
 
         context_ = tf.matmul(h, self.C, transpose_b=True)
@@ -57,19 +57,20 @@ class Transceducer:
 
 
 if __name__ == '__main__':
+
     sess = tf.Session()
 
-    memory_size = 100
+    memory_size = 10
     context_size = 20
     recall_size = 10
 
-    bnet = Transceducer(memory_size, context_size, recall_size)
+    bnet = Transceducer(memory_size, context_size, recall_size, 0.5)
 
     contexts = []
     inputs = []
     outputs = []
     ops = []
-    for i in xrange(10):
+    for i in xrange(memory_size):
         context = tf.constant(np.random.rand(1, context_size) - 0.5, dtype=tf.float32)
         input = tf.constant(np.random.rand(1, recall_size) - 0.5, dtype=tf.float32)
         output = bnet.infer(tf.nn.dropout(context, 0.5))
@@ -84,7 +85,7 @@ if __name__ == '__main__':
     sess.run(tf.global_variables_initializer())
 
     reseed = bnet.get_reseed_operation()
-    for i in xrange(10):
+    for i in xrange(memory_size):
         # shift memory anchor
         for j in xrange(100):
             print sess.run(ops[i])
@@ -92,5 +93,5 @@ if __name__ == '__main__':
 
     # sess.run(bnet.get_reset_operation())
 
-    for i in xrange(10):
+    for i in xrange(memory_size):
         print sess.run(tf.reduce_sum(tf.squared_difference(outputs[i], inputs[i])))
